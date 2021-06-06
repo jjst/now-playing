2e78b94ed6507302aa7c6ea8d11af1bccf47e93b
Jeremie Jost <jeremie.jost@zalando.ie>
2021-06-06 21:13:51 +0200

    Add all autogenerated code

---
+++ swagger_server/controllers/stations_controller.py
@@ -0,0 +1,34 @@
+import connexion
+import six
+
+from swagger_server.models.radio_station import RadioStation  # noqa: E501
+from swagger_server.models.search_result import SearchResult  # noqa: E501
+from swagger_server import util
+
+
+def get_station_by_country_code_and_station_id(countryCode, stationId):  # noqa: E501
+    """Find pet by ID
+
+    Returns a radio station # noqa: E501
+
+    :param countryCode: Country code of a station
+    :type countryCode: str
+    :param stationId: ID of a station
+    :type stationId: str
+
+    :rtype: RadioStation
+    """
+    return 'do some magic!'
+
+
+def search(query):  # noqa: E501
+    """Finds a station by name
+
+    Muliple tags can be provided with comma separated strings. Use         tag1, tag2, tag3 for testing. # noqa: E501
+
+    :param query: Tags to filter by
+    :type query: List[str]
+
+    :rtype: List[SearchResult]
+    """
+    return 'do some magic!'
---
+++ swagger_server/test/test_stations_controller.py
@@ -0,0 +1,43 @@
+# coding: utf-8
+
+from __future__ import absolute_import
+
+from flask import json
+from six import BytesIO
+
+from swagger_server.models.radio_station import RadioStation  # noqa: E501
+from swagger_server.models.search_result import SearchResult  # noqa: E501
+from swagger_server.test import BaseTestCase
+
+
+class TestStationsController(BaseTestCase):
+    """StationsController integration test stubs"""
+
+    def test_get_station_by_country_code_and_station_id(self):
+        """Test case for get_station_by_country_code_and_station_id
+
+        Find pet by ID
+        """
+        response = self.client.open(
+            '/api/stations/{countryCode}/{stationId}'.format(countryCode='countryCode_example', stationId='stationId_example'),
+            method='GET')
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+    def test_search(self):
+        """Test case for search
+
+        Finds a station by name
+        """
+        query_string = [('query', 'query_example')]
+        response = self.client.open(
+            '/api/search',
+            method='GET',
+            query_string=query_string)
+        self.assert200(response,
+                       'Response body is : ' + response.data.decode('utf-8'))
+
+
+if __name__ == '__main__':
+    import unittest
+    unittest.main()
