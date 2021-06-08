83c89c193284bd6c15f868e0ddc834e2ede9df35
Jeremie Jost <jeremiejost@gmail.com>
2021-06-08 22:15:38 +0200

    FRANCEBLEUFRANCEBLEUFRANCEBLEU

--- aggregators/france_inter.py
+++ aggregators/france_inter.py
@@ -1,26 +1,29 @@
 from datetime import datetime
 
 from aggregators import PlayingItem
+import logging
 
-stations = {
-    "france-inter": "https://www.franceinter.fr/programmes",
-    "france-bleu-alsace": "https://www.francebleu.fr/grid/alsace",
-    "france-bleu-belfort-montbeliard": "https://www.francebleu.fr/grid/belfort-montbeliard",
-    "france-bleu-armorique": "https://www.francebleu.fr/grid/armorique"
-}
+url = "https://www.franceinter.fr/programmes"
 
 
 def fetch(session, request_type, country_code, station_id):
-    url = stations[station_id]
+    return fetch_url(session, url, country_code, station_id)
+
+
+def fetch_url(session, url, country_code, station_id):
     response = session.get(
         url=url,
         params={'xmlHttpRequest': 1, 'ignoreGridHour': 1}
     )
     now = datetime.now()
     playlist = response.json()
+    print(playlist)
     for item in playlist:
         start = datetime.fromtimestamp(item['start'])
         end = datetime.fromtimestamp(item['end'])
+        if now <= end:
+            logging.debug(f'{start} -> {end}: {item["conceptTitle"]} - {item["expressionTitle"]}')
+            print(now >= start)
         if now >= start and now <= end:
             if item['conceptTitle'] == item['expressionTitle']:
                 title = item['conceptTitle']
