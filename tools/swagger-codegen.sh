eb513d89fd3edd4db967428bce0510f95be31ea1
Jeremie Jost <jeremiejost@gmail.com>
2021-06-09 09:43:15 +0200

    swagger -> openapi

--- tools/swagger-codegen.sh
+++ tools/swagger-codegen.sh
@@ -4,7 +4,7 @@ set -e -u
 mkdir -p /tmp/swagger-codegen
 docker run \
     -u $(id -u ${USER}):$(id -g ${USER}) \
-    -v "$(pwd)"/api/swagger:/swagger \
+    -v "$(pwd)"/api/openapi:/openapi \
     -v "/tmp/swagger-codegen:/tmp" \
-    swaggerapi/swagger-codegen-cli generate -l python-flask -i /swagger/swagger.yaml -o /tmp/ -Dmodels -DpackageName=api
+    swaggerapi/swagger-codegen-cli generate -l python-flask -i /openapi/spec.yaml -o /tmp/ -Dmodels -DpackageName=api
 cp -R /tmp/swagger-codegen/* .
