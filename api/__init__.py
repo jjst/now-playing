4e847304228ed06b36d622ee5297f358bc773f34
Jeremie Jost <jeremiejost@gmail.com>
2021-06-10 08:59:59 +0200

    Instrument service using opentelemetry

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,11 @@
 import connexion
 
 from api import encoder
+from api.tracing import configure_tracer
 
 
 app = connexion.FlaskApp(__name__, specification_dir='./openapi/')
 app.app.json_encoder = encoder.JSONEncoder
 app.add_api('spec.yaml', arguments={'title': 'Now Playing API'})
+
+configure_tracer(app)
