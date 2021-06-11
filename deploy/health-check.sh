5449c8cc6cd52b9e5dc5561b9750731b5919be5b
Jérémie Jost <jeremiejost@gmail.com>
2021-06-11 12:59:45 +0200

    Health check job (#31)
    * Install dependencies for health check
    
    * Move health check to script
    
    * Run on healthcheck change
    
    * Debug script
    
    * Change watched path
    
    * Debug script
    
    * More debugging
    
    * Specify GET
    
    * more GET

---
+++ deploy/health-check.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo $APP_HOSTNAME/api/stations/$NAMESPACE/
+http --verbose GET "$APP_HOSTNAME/api/stations/$NAMESPACE/"
+json=$(http GET "$APP_HOSTNAME/api/stations/$NAMESPACE/")
+echo $json
+stations=$(echo $json | jq -r '.items | .[] | .id')
+echo $stations
+for id in $stations; do
+	http GET $APP_HOSTNAME/api/stations/$NAMESPACE/$id/now-playing
+done
