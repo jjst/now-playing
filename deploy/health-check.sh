6c4716857d79c5ca562d9fef4c2a6fe13c68a336
Jeremie Jost <jeremiejost@gmail.com>
2021-06-11 12:57:45 +0200

    more GET

--- deploy/health-check.sh
+++ deploy/health-check.sh
@@ -6,5 +6,5 @@ echo $json
 stations=$(echo $json | jq -r '.items | .[] | .id')
 echo $stations
 for id in $stations; do
-	http $APP_HOSTNAME/api/stations/$NAMESPACE/$id/now-playing
+	http GET $APP_HOSTNAME/api/stations/$NAMESPACE/$id/now-playing
 done
