c8150857f52f6c193b2aa1a100ddf7aeea2abe2b
Jeremie Jost <jeremiejost@gmail.com>
2021-06-11 12:32:49 +0200

    Debug script

--- deploy/health-check.sh
+++ deploy/health-check.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-stations=$(http $APP_HOSTNAME/api/stations/$NAMESPACE/ | jq -r '.items | .[] | .id')
+json=$(http $APP_HOSTNAME/api/stations/$NAMESPACE/)
+echo $json
+stations=$(echo $json | jq -r '.items | .[] | .id')
 echo $stations
 for id in $stations; do
 	http $APP_HOSTNAME/api/stations/$NAMESPACE/$id/now-playing
