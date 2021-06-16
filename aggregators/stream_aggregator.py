8dd9b998c4d410ee1aeb96eb0334ad13b1081c0d
Jeremie Jost <jeremiejost@gmail.com>
2021-06-16 23:06:07 +0200

    Use chardet to detect encoding

--- aggregators/stream_aggregator.py
+++ aggregators/stream_aggregator.py
@@ -1,18 +1,25 @@
 from aggregators import PlayingItem
+
+import chardet
+import logging
 from streamscrobbler import streamscrobbler
 
 
-def fetch(session, request_type: str, stream_url: str, encoding: str = 'utf-8'):
+def fetch(session, request_type: str, stream_url: str, encoding: str = None):
     stationinfo = streamscrobbler.get_server_info(stream_url)
     if stationinfo['metadata']:
         metadata = stationinfo['metadata']
         try:
             song = metadata['song']
-            try:
-                # Looks like sometimes the library returns bytes instead of str...
+            # Looks like sometimes the library returns bytes instead of str...
+            # If this is true we got ourselves a bytes/bytestring object
+            if hasattr(song, 'decode'):
+                if not encoding:
+                    logging.warn("Encoding not specified, trying to guess charset...")
+                    result = chardet.detect(song)
+                    logging.info(f"Detected: {result}")
+                    encoding = result['encoding']
                 song = song.decode(encoding)
-            except AttributeError:
-                pass
             return [PlayingItem(type='song', title=song)]
         except KeyError:
             return []
