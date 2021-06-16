e4e29a97a22ddd182db188e14cd7bc85d7eeaf9b
Jeremie Jost <jeremiejost@gmail.com>
2021-06-16 18:21:24 +0200

    bytestring workaround

--- aggregators/stream_aggregator.py
+++ aggregators/stream_aggregator.py
@@ -2,12 +2,17 @@ from aggregators import PlayingItem
 from streamscrobbler import streamscrobbler
 
 
-def fetch(session, request_type: str, stream_url: str):
+def fetch(session, request_type: str, stream_url: str, encoding: str = 'utf-8'):
     stationinfo = streamscrobbler.get_server_info(stream_url)
     if stationinfo['metadata']:
         metadata = stationinfo['metadata']
         try:
             song = metadata['song']
+            try:
+                # Looks like sometimes the library returns bytes instead of str...
+                song = song.decode(encoding)
+            except AttributeError:
+                pass
             return [PlayingItem(type='song', title=song)]
         except KeyError:
             return []
