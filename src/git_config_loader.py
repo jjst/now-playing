5dda25f4b815faac6e3012e8c6eb21d0aa8d759c
Jeremie Jost <jeremiejost@gmail.com>
2021-06-18 10:11:55 +0200

    Schedule regular check of remote in git_config_loader

--- src/git_config_loader.py
+++ src/git_config_loader.py
@@ -1,6 +1,7 @@
 from dynaconf.default_settings import get
 from dynaconf.loaders import yaml_loader
 import os
+from apscheduler.schedulers.background import BackgroundScheduler
 import subprocess
 import tempfile
 
@@ -10,11 +11,28 @@ TEMP_DIR = tempfile.gettempdir()
 CONFIG_REPOSITORY_PATH = os.path.join(TEMP_DIR, "git-config")
 
 
-def clone_and_watch_repo(repo, subfolder=None):
+def clone_and_watch_repo(repo, subfolder, frequency):
+    print("Cloning repo")
     subprocess.run(["git", "clone", "--no-checkout", "--depth=1", repo, CONFIG_REPOSITORY_PATH], cwd=TEMP_DIR)
     if subfolder:
         subprocess.run(["git", "sparse-checkout", "init", "--cone"], cwd=CONFIG_REPOSITORY_PATH)
         subprocess.run(["git", "sparse-checkout", "set", subfolder], cwd=CONFIG_REPOSITORY_PATH)
+    print("Starting scheduled checks")
+    sched = BackgroundScheduler(daemon=True)
+    sched.add_job(lambda: check_for_changes(repo, subfolder), 'interval', seconds=frequency)
+    sched.start()
+
+
+def check_for_changes(repo, subfolder):
+    print("Checking repo for changes...")
+    subprocess.run(["git", "fetch"], cwd=CONFIG_REPOSITORY_PATH)
+    if subfolder:
+        extra_args = ["--", subfolder]
+    else:
+        extra_args = []
+    changed = subprocess.check_output(["git", "diff", "--name-only", "main", "origin/main"] + extra_args, cwd=CONFIG_REPOSITORY_PATH)
+    changed = changed.decode('utf-8').strip().split("\n")
+    print(changed)
 
 
 def load(obj, env=None, silent=True, key=None, filename=None):
@@ -31,7 +49,13 @@ def load(obj, env=None, silent=True, key=None, filename=None):
     # use `obj.set(key, value)` or `obj.update(dict)` to load data
     # use `obj.find_file('filename.ext')` to find the file in search tree
     # Return nothing
-    repo = get("GIT_REPO_FOR_DYNACONF")
-    subfolder = get("GIT_REPO_SUBFOLDER_FOR_DYNACONF", None)
-    if repo and not os.path.exists(CONFIG_REPOSITORY_PATH):
-        clone_and_watch_repo(repo, subfolder)
+    print(f"In load obj={obj} key={key}")
+
+
+repo = get("GIT_REPO_FOR_DYNACONF")
+subfolder = get("GIT_REPO_SUBFOLDER_FOR_DYNACONF", None)
+frequency = int(get("GIT_REPO_CHECK_FREQUENCY_SECONDS_FOR_DYNACONF", 10))
+if os.path.exists(CONFIG_REPOSITORY_PATH):
+    print(f"'{CONFIG_REPOSITORY_PATH}' is not empty, skipping")
+if repo and not os.path.exists(CONFIG_REPOSITORY_PATH):
+    clone_and_watch_repo(repo, subfolder, frequency=frequency)
