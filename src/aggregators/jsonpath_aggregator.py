9aeff3c6a95d7bda7610005239d9ea78ff436ebd
Jérémie Jost <jeremiejost@gmail.com>
2021-06-21 17:20:52 +0200

    Parse spin 1038 (#57)
    * Parse spin 1038
    
    * Remove local test url
    
    * Remove print() statements

--- src/aggregators/jsonpath_aggregator.py
+++ src/aggregators/jsonpath_aggregator.py
@@ -1,5 +1,7 @@
 from jsonpath_ng.ext import parse
+import commentjson
 import json
+from json.decoder import JSONDecodeError
 import pprint
 import logging
 
@@ -15,7 +17,8 @@ JAVA_JSONPATH_API_URL = "https://java-jsonpath-api-bknua.ondigitalocean.app/"
 
 def fetch(session, request_type: str, url: str, field_extractors: dict, format_string: str, engine: str = DEFAULT_ENGINE):
     response = session.get(url)
-    extracted_json_by_json_query = extract_json(session, field_extractors.values(), response, engine)
+    json_data = read_json(response)
+    extracted_json_by_json_query = extract_json(session, field_extractors.values(), json_data, engine)
     field_values = {name: extracted_json_by_json_query[query] for (name, query) in field_extractors.items()}
     logging.debug(field_values)
     title = format_string.format(**field_values)
@@ -23,18 +26,22 @@ def fetch(session, request_type: str, url: str, field_extractors: dict, format_s
     return playing_items
 
 
-def extract_json(session, jsonpath_queries, response, engine=DEFAULT_ENGINE):
+def read_json(response):
+    return commentjson.loads(response.text)
+
+
+def extract_json(session, jsonpath_queries, json_data, engine=DEFAULT_ENGINE):
     if engine == PYTHON_JSONPATH_NG_EXT:
         query_results = {}
         for query in jsonpath_queries:
             jsonpath_expr = parse(query)
-            matches = jsonpath_expr.find(response.json())
+            matches = jsonpath_expr.find(json_data)
             query_results[query] = matches[0]
     elif engine == JAVA_JAYWAY:
-        query_results = query_java_jsonpath_api(session, list(jsonpath_queries), response.text)
+        query_results = query_java_jsonpath_api(session, list(jsonpath_queries), json.dumps(json_data))
         logging.debug("Extraction results from API:")
         logging.debug(f"\n{pprint.pformat(query_results)}\n")
-        return {query: results[0] for (query, results) in query_results.items()}
+        return {query: r if isinstance(r, str) else r[0] for (query, r) in query_results.items()}
     else:
         raise ValueError(f"Invalid jsonpath engine value: '{engine}'")
     return query_results
@@ -46,8 +53,12 @@ def query_java_jsonpath_api(session, jsonpath_queries, json_str):
         'json_str': json_str
     }
     response = session.post(JAVA_JSONPATH_API_URL, json=request_body)
-    results = response.json()
-    logging.debug("API response:")
-    logging.debug(results)
+    try:
+        results = response.json()
+        logging.debug("API response:")
+        logging.debug(results)
+    except JSONDecodeError:
+        logging.error("API response:")
+        logging.error(response.text)
     # JSON response for each result is stringified so it needs to be JSON decoded again
     return {jsonpath_query: json.loads(result) for jsonpath_query, result in results.items()}
