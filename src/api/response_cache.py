caad59684f8debed2b63c94888f6c5db32ed88a4
Jérémie Jost <jeremiejost@gmail.com>
2021-07-06 14:55:16 +0200

    Smarter caching (fixes #92) (#93)
    * Add xxhash dependency, add redis key prefixes
    
    * Implement smart caching + tests (fixes #92)
    
    * Tracing
    
    * More tests

--- src/api/response_cache.py
+++ src/api/response_cache.py
@@ -1,40 +1,52 @@
 import logging
 import redis
+import xxhash
 from datetime import datetime, timedelta
 from typing import Optional
+from opentelemetry import trace
 
 from base.config import settings
 from base.stations import RadioStationInfo
 
+response_prefix = "response"
+response_hash_prefix = "response-hash"
+
+
+def key_for(station, prefix):
+    return f"{prefix}:{station.station_id()}"
+
 
 class ResponseCache():
     def __init__(self, settings=settings):
-        self.redis_client = redis.Redis(
-            settings.redis.host,
-            settings.redis.port,
-            settings.redis.db,
-            socket_timeout=settings.redis.socket_timeout,
-            socket_connect_timeout=settings.redis.socket_connect_timeout,
-        )
-        self.default_ttl_seconds = settings.redis.default_ttl_seconds
+        self.redis_client = redis.from_url(settings.redis.url, **settings.redis.args)
+        self.default_ttl_seconds = settings.response_cache.default_ttl_seconds
+        self.default_ttl_seconds_if_changed = settings.response_cache.default_ttl_seconds_if_changed
 
     def get(self, station: RadioStationInfo) -> Optional[str]:
         try:
-            return self.redis_client.get(station.station_id())
+            return self.redis_client.get(key_for(station, 'response'))
         except redis.exceptions.ConnectionError as e:
             logging.warning("Error connecting to Redis. Server cannot return cached response.")
             logging.exception(e)
             return None
 
     def set(self, station: RadioStationInfo, response: str, expire_in: Optional[int] = None, expire_at: Optional[datetime] = None):
+        if expire_in and expire_at:
+            raise ValueError("Only one of 'expire_in' or 'expire_at' should be provided as argument")
+        current_span = trace.get_current_span()
+        new_hashed_response = xxhash.xxh32_digest(response)
+        old_hashed_response = self.redis_client.getset(key_for(station, 'response-hash'), new_hashed_response)
         if expire_in:
             ttl_seconds = expire_in
         elif expire_at:
             ttl_seconds = max(self.default_ttl_seconds, int((expire_at - datetime.now()).total_seconds()))
+        elif old_hashed_response and new_hashed_response != old_hashed_response:
+            ttl_seconds = self.default_ttl_seconds_if_changed
         else:
             ttl_seconds = self.default_ttl_seconds
+        current_span.add_event("determine_cached_response_ttl", attributes={'ttl_seconds', ttl_seconds})
         try:
-            self.redis_client.set(station.station_id(), response, ex=ttl_seconds)
+            self.redis_client.set(key_for(station, 'response'), response, ex=ttl_seconds)
         except redis.exceptions.ConnectionError as e:
             logging.warning("Error connecting to Redis. Server cannot return cached response.")
             logging.exception(e)
