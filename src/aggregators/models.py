943cf6bdf69747ed352f7744890d5c8b5d15387f
Jérémie Jost <jeremiejost@gmail.com>
2021-07-14 22:40:36 +0200

    Jazz radio (#108)
    * Add first station generated by add_station.py
    
    * Fix album field
    
    * flake8 fix
    
    * Fix stream_aggregator edge case

--- src/aggregators/models.py
+++ src/aggregators/models.py
@@ -40,11 +40,13 @@ class Song(PlayingItem):
     song_title: str
     album: Optional[str]
 
-    def __init__(self, artist: str, song_title: str, start_time: OptionalTime = None, end_time: OptionalTime = None):
+    def __init__(self, artist: str, song_title: str, album: Optional[str] = None,
+                 start_time: OptionalTime = None, end_time: OptionalTime = None):
         text = f"{artist} - {song_title}"
         super().__init__(type='song', text=text, start_time=start_time, end_time=end_time)
         self.artist = artist
         self.song_title = song_title
+        self.album = album
 
 
 class Programme(PlayingItem):
