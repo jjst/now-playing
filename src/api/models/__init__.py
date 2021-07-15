b12a9b5bf3c4625604190f0d6bfde80bee94f300
Jeremie Jost <jeremiejost@gmail.com>
2021-07-15 10:11:50 +0200

    Enrich now playing api response

--- src/api/models/__init__.py
+++ src/api/models/__init__.py
@@ -3,8 +3,12 @@
 # flake8: noqa
 from __future__ import absolute_import
 # import models into model package
-from api.models.now_playing import NowPlaying
+from api.models.now_playing_item import NowPlayingItem
+from api.models.now_playing_item_list import NowPlayingItemList
+from api.models.one_of_now_playing_item_list_items_items import OneOfNowPlayingItemListItemsItems
+from api.models.programme import Programme
 from api.models.radio_station import RadioStation
 from api.models.radio_station_list import RadioStationList
 from api.models.search_result import SearchResult
+from api.models.song import Song
 from api.models.stream import Stream
